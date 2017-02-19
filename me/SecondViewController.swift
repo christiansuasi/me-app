8af53daeafd4d2ff8f83e936988ff3e4d2bded0b
Christian <christiansuasi@gmail.com>
2017-02-19 17:41:32 -0500

    fix tab bar controller navi

--- me/SecondViewController.swift
+++ me/SecondViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    
+    
+    //MARK: Properties
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,9 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: Actions
 
-
+    
 }
 
