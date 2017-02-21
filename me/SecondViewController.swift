e23c4cd8b8f9e1d877af153784f3e30bad4d33d1
Christian <christiansuasi@gmail.com>
2017-02-20 19:23:50 -0500

    upload image doneish

--- me/SecondViewController.swift
+++ me/SecondViewController.swift
@@ -8,15 +8,18 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     
     
     //MARK: Properties
+    var imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var chosenImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,8 +27,58 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    //MARK: Actions
+    
+    @IBAction func pickFromGallery(_ sender: UIButton) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
+            
+            imagePicker.delegate = self;
+            imagePicker.sourceType = .savedPhotosAlbum;
+            imagePicker.allowsEditing = true;
+            
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            chosenImage.image = image
+        } else{
+            print("Something went wrong")
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
 
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: { () -> Void in
+            
+        })
+        
+    }
+    
+    @IBAction func openCamera(sender: AnyObject) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera;
+            imagePicker.allowsEditing = false
+            
+            self.present(imagePicker, animated: true, completion: nil)
+            
+        }else{
+            print("Camera not available")
+        }
+        
+    }
+    
+    
+    
+   
+    //MARK: Actions
     
 }
 
